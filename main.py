95141116dbae1504c2631ea05bd681d19bbbca8c
catherine232 <134084724+catherine232@users.noreply.github.com>
2023-05-23 10:50:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app=FastAPI()
 
@@ -6,11 +7,10 @@ app=FastAPI()
 def index():
     return "Hola a todos, quieres saber de perritos"
 @app.get("/Perrito/{num}")
-def perrito(num):
+def perritos(num):
     perritos={
     "1": "Chihuahua",
     "2": "Pitbul"
-    
     }
     return perritos{num} 
 @app.get("/Conversor_CaF/{C}")
@@ -22,4 +22,27 @@ def conversorCaF(C):
             return f"La temperatura es de {TF} grados Farenheit"
     except:
             return "Entrada invalida"
+@app.get("/RevisarEdad/{E1}/{E2}")
+def revisar_edades(E1,E2):
+    E1=int(E1)
+    E2=int(E2)
+    if E1>E2=30:
+        return "Podrias ser su abuelo"
+    elif E1>E2+15:
+        return "Podrias ser su padre"
+    elif E1>E2:
+        return "Eres mayor"
+    if E2>E1=30:
+        return "Podria ser tu abuelo"
+    elif E2>E1+15:
+        return "Podria ser tu padre"
+    elif E2>E1:
+        return "Es mayor que tu"
+    else:
+        return "Tienen la misma edad"
+    
+
+    
+    
+    
 
